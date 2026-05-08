5cf1270dfdc9293e162ef2c0296920487564e0d9
JuniperBerryTX <42944693+JuniperBerryTX@users.noreply.github.com>
2026-05-08 18:27:56 -0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    __AIRTABLE_KEY__: JSON.stringify(process.env.VITE_AIRTABLE_KEY || ''),
-  }
-})
